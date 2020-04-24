11ae7f9117294edfe69c9b3331057a43e41c0bb0
Paul O'Leary McCann <polm@dampfkraft.com>
2020-04-24 16:52:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# kanji 漢字
+
+Kanji is a package for getting information about Chinese characters as used in
+Japanese.
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kanji',
+    version='0.0.0',
+    url='https://github.com/polm/kanji.git',
+    author="Paul O'Leary McCann",
+    author_email='polm@dampfkraft.com',
+    description='Japanese kanji data',
+    packages=find_packages(),    
+    install_requires=[],
+)
